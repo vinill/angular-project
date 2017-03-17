7907167f110406e13d7ae1742ecb5ae445270503
vinilll <dzisvolodymyr@gmail.com>
2017-03-17 01:40:17 +0100

    Components

--- app/app.js
+++ app/app.js
@@ -1,19 +1,4 @@
 'use strict';
 
-var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListController', function PhoneListController($scope) {
-  $scope.phones = [
-    {
-      name: 'Nexus S',
-      snippet: 'Fast just got faster with Nexus S.'
-    }, {
-      name: 'Motorola XOOM™ with Wi-Fi',
-      snippet: 'The Next, Next Generation tablet.'
-    }, {
-      name: 'MOTOROLA XOOM™',
-      snippet: 'The Next, Next Generation tablet.'
-    }
-  ];
-  $scope.name = 'world';
-});
+angular.module('phonecatApp', []);
