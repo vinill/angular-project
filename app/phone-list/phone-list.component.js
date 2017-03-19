c0a5a7c89b15431821e9a94ccdbf08b9ff740d33
vinilll <dzisvolodymyr@gmail.com>
2017-03-19 20:53:27 +0100

    Directory and File Organization

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -1,15 +1,10 @@
 'use strict';
 
+// Register `phoneList` component, along with its associated controller and template
 angular.
-  module('phonecatApp').
+  module('phoneList').
   component('phoneList', {
-    template:
-        '<ul>' +
-          '<li ng-repeat="phone in $ctrl.phones">' +
-            '<span>{{phone.name}}</span>' +
-            '<p>{{phone.snippet}}</p>' +
-          '</li>' +
-        '</ul>',
+    templateUrl: 'phone-list/phone-list.template.html',
     controller: function PhoneListController() {
       this.phones = [
         {
