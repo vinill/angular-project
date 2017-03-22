9b391a2cac2edda363d47cc30ed69f7f147da09f
vinilll <dzisvolodymyr@gmail.com>
2017-03-22 22:59:40 +0100

    More templating

--- app/phone-list/phone-list.component.spec.js
+++ app/phone-list/phone-list.component.spec.js
@@ -1,34 +1,28 @@
 'use strict';
 
-describe('phoneList', function() {
+describe('phoneDetail', function() {
 
-  // Load the module that contains the `phoneList` component before each test
-  beforeEach(module('phoneList'));
+  // Load the module that contains the `phoneDetail` component before each test
+  beforeEach(module('phoneDetail'));
 
   // Test the controller
-  describe('PhoneListController', function() {
+  describe('PhoneDetailController', function() {
     var $httpBackend, ctrl;
 
-    // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
-    // This allows us to inject a service and assign it to a variable with the same name
-    // as the service while avoiding a name conflict.
-    beforeEach(inject(function($componentController, _$httpBackend_) {
+    beforeEach(inject(function($componentController, _$httpBackend_, $routeParams) {
       $httpBackend = _$httpBackend_;
-      $httpBackend.expectGET('phones/phones.json')
-                  .respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+      $httpBackend.expectGET('phones/xyz.json').respond({name: 'phone xyz'});
 
-      ctrl = $componentController('phoneList');
+      $routeParams.phoneId = 'xyz';
+
+      ctrl = $componentController('phoneDetail');
     }));
 
-    it('should create a `phones` property with 2 phones fetched with `$http`', function() {
-      expect(ctrl.phones).toBeUndefined();
+    it('should fetch the phone details', function() {
+      expect(ctrl.phone).toBeUndefined();
 
       $httpBackend.flush();
-      expect(ctrl.phones).toEqual([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
-    });
-
-    it('should set a default value for the `orderProp` property', function() {
-      expect(ctrl.orderProp).toBe('age');
+      expect(ctrl.phone).toEqual({name: 'phone xyz'});
     });
 
   });
