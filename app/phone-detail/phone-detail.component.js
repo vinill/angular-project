f074bd7be8384a19345252773ba8bca614690d32
vinilll <dzisvolodymyr@gmail.com>
2017-03-23 12:26:41 +0100

    REST and custom services

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -4,13 +4,16 @@ angular.
   module('phoneDetail').
   component('phoneDetail', {
     templateUrl: 'phone-detail/phone-detail.template.html',
-    controller: ['$http', '$routeParams',
-      function PhoneDetailController($http, $routeParams) {
+    controller: ['$routeParams', 'Phone',
+      function PhoneDetailController($routeParams, Phone) {
         var self = this;
-
-        $http.get('phones/' + $routeParams.phoneId + '.json').then(function(response) {
-          self.phone = response.data;
+        self.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+          self.setImage(phone.images[0]);
         });
+
+        self.setImage = function setImage(imageUrl) {
+          self.mainImageUrl = imageUrl;
+        };
       }
     ]
   });
